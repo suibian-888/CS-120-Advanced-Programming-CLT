eace39a242de5664131ac165dd60b1a7a7d6b325
chloe hoff <98049623+chloedhoff@users.noreply.github.com>
2023-03-21 10:58:43 -0400

    fixed bugs

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -3,11 +3,11 @@ public class sum_even_fibonacci {
     public static void main(String[] args) {
         int a = 0, b = 1;
         int sumEven = 0;
-        while (b > 4000000) {
+        while (b < 4000000) {
             if (b % 2 == 0) {
                 sumEven += b;
             }
-            int temp == b;
+            int temp = b;
             b = a + b;
             a = temp;
         }
