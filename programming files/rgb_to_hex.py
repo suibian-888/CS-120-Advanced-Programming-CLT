0cebec72be65aa5cfea5e57a6219e9528a4070a0
lilyycarr <112419791+lilyycarr@users.noreply.github.com>
2023-03-21 10:35:21 -0400

    Update rgb_to_hex.py

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,7 +1,8 @@
-def rgb_to_hex(r, g, b):
+#added bugs: lacarr
+def rgb_to_hex(r, g, b)
     r = max(0, min(255, r))
     g = max(0, min(255, g))
-    b = max(0, min(255, b))
+    b  max(0, min(255, b))
     return '{:02X}{:02X}{:02X}'.format(r, g, b)
 
 
